28c487b192a9c0bd919b2cbc18412e8f7c786fad
TMP-tenpura <56856373+TMP-tenpura@users.noreply.github.com>
2020-09-09 18:02:20 +0900

    Update GenLib.py

--- GenLib.py
+++ GenLib.py
@@ -1,4 +1,3 @@
-import re
 tags = ['a','abbr','address','article','aside','audio','b','bdo','blockquote','body','button','canvas','caption','cite','cite','code','colgroup','command','datalist','dd','del','details','dfn','div','dl','dt','em','fieldset','figcaption','figure','footer','form','h1','h2','h3','h4','h5','h6','head','header','html','i','iframe','ins','kbd','label','legend','li','map','main','mark','menu','meter','nav','noscript','object','ol','optgroup','option','output','p','pre','progress','q','rp','rt','ruby','s','samp','script','section','select','small','span','strong','style','sub','summary','sup','table','tbody','td','textarea','tfoot','th','thead','time','title','tr','ul','var','video']
 nctags = ['br','img','hr','meta','input','embed','area','base','col','keygen','link','param','source']
 with open('ht.py', mode='w') as f:
