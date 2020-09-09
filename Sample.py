c0a396bc5c24f28a438c1234613536b04e5f8d91
TMP-tenpura <56856373+TMP-tenpura@users.noreply.github.com>
2020-09-09 18:21:27 +0900

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,13 @@
+import ht
+result='https://google.com'
+print(
+ht.head(
+	ht.title('test')
+	+ht.link(rel='stylesheet',href='style.css')
+	)
++ht.body(
+	ht.h1('test')
+	+'this is test page\n'
+	+ht.a('link',href='https://google.com')
+	)
+)
